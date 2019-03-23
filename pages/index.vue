13fa6a8e938e29e33ba7b34f0440021be0c10f99
Vítor Marçal <vitormarcal8@gmail.com>
2019-03-23 10:49:54 -0300

    delete logo padrão

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="container">
     <div>
-      <logo />
       <h1 class="title">
         vitormarcal
       </h1>
@@ -25,11 +24,9 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    Logo
   }
 }
 </script>
