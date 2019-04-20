f6d8a28407f259aab890b853f3440f94fb03c56b
Vítor Marçal <git@vitormarcal.com.br>
2019-04-20 14:30:10 -0300

    build

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,15 @@
 <template>
   <div>
     <main>
-      <c-header/>
-      <transition name="layout" mode="out-in">
         <nuxt />
-      </transition>
     </main>
   </div>
 </template>
 <script>
   export default {
     components: {
-      CHeader: () => import('~/components/Header'),
-      CFooter: () => import('~/components/Footer')
     }
   }
 </script>
-<style lang="scss">
+<style>
 </style>
