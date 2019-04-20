d0305da26d68b5d43d6c963537b7ae29de2e0429
Vítor Marçal <git@vitormarcal.com.br>
2019-04-20 13:17:12 -0300

    new index page

---
+++ components/SocialIcons.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="social_icons">
+    <a href="mailto:vitormarcal8@gmail.com" title="Email me" target="_blank" rel="noopener noreferrer">
+      <img src="~/assets/images/mailme.svg" alt="Email me "/>
+    </a>
+    <a href="https://github.com/vitormarcal" title="Meu github" target="_blank" rel="noopener noreferrer">
+      <img src="~/assets/images/github-logo.svg" alt="Meu github"/>
+    </a>
+    <a href="https://www.linkedin.com/in/vitormarcal/" title="Meu Linkedin" target="_blank" rel="noopener noreferrer">
+      <img src="~/assets/images/linkedin-logo.svg" alt="Meu Linkedin" />
+    </a>
+  </div>
+</template>
+<script>
+  export default {
+  }
+</script>
+<style scoped lang="scss">
+
+  .social_icons {
+    padding-top: 2rem;
+  }
+
+</style>
