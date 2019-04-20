d3d32e5b42672480f68b32ab10f1a6e3d2877e61
Vítor Marçal <git@vitormarcal.com.br>
2019-04-20 16:39:22 -0300

    add morgan

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const compression = require('compression');
+const morgan = require('morgan');
 const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT = 3000;
 
 app.use(compression());
+app.use(morgan('tiny'))
 app.use(express.static('dist'));
 
 
