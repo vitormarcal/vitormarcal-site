1d99336a291b5ad1086ff1035113e2da882c255c
Vítor Marçal <git@vitormarcal.com.br>
2019-04-20 23:54:32 -0300

    add algumas metas

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,16 @@ export default {
     title: pkg.name,
     meta: [
       { charset: 'utf-8' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+      { 'http-equiv': 'Content-Language', content: 'pt-br' },
+      { name: 'og:locale', content: 'pt_BR' },
+      { name: 'country', content: 'Brazil' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'mobile-web-app-capable', content: 'yes' },
+      { name: 'googlebot', content: 'index,follow' },
+      { name: 'robots', content: 'index,follow,noodp' },
+      { name: 'revisit-after', content: '7 days' },
+      { hid: 'author', name: 'author', content: pkg.author },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
