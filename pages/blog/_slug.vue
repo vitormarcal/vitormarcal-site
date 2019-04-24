e60a59a408130ea097314ff5c095359885328e6a
Vítor Marçal <git@vitormarcal.com.br>
2019-04-24 00:00:44 -0300

    definido estilos bases para titulo e paragrafo

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,6 +1,7 @@
 <template>
       <div class="container">
-        <div v-html="content"></div>
+        <div v-html="content" class="content">
+        </div>
       </div>
 </template>
 <script>
@@ -15,10 +16,27 @@
 </script>
 
 <style  lang="scss">
-  .container {
+  .content {
     max-width: 700px;
     padding-left: 20px;
     padding-right: 20px;
     margin: 0 auto;
+
+    h1 {
+      font-family: 'Amiri', serif;
+      font-weight: 400;
+      font-style: normal;
+      line-height: 1.25;
+      letter-spacing: 0;
+    }
+
+    p {
+      font-family: 'Open Sans', serif;
+      font-weight: 400;
+      font-style: normal;
+      line-height: 1.58;
+      letter-spacing: -.003em;
+      font-size: 21px;
+    }
   }
 </style>
