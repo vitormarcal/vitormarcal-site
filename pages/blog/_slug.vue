0ed87be1224eca8234fe538607904a3ec623fba1
Vítor Marçal <git@vitormarcal.com.br>
2019-04-24 12:56:12 -0300

    redefinindo folhas de estilo da pagina inicial

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -21,22 +21,5 @@
     padding-left: 20px;
     padding-right: 20px;
     margin: 0 auto;
-
-    h1 {
-      font-family: 'Amiri', serif;
-      font-weight: 400;
-      font-style: normal;
-      line-height: 1.25;
-      letter-spacing: 0;
-    }
-
-    p {
-      font-family: 'Open Sans', serif;
-      font-weight: 400;
-      font-style: normal;
-      line-height: 1.58;
-      letter-spacing: -.003em;
-      font-size: 21px;
-    }
   }
 </style>
