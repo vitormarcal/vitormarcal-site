e5f8b1af1af3794e7b9daeeaeccbe1709435bd8e
Vítor Marçal <git@vitormarcal.com.br>
2019-04-25 22:52:01 -0300

    add base navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+      <ul>
+        <li><nuxt-link to="/" title="Página inicial">inicio</nuxt-link></li>
+        <li><nuxt-link to="/sobre" title="Sobre mim">sobre</nuxt-link></li>
+        <li><nuxt-link to="/contato" title="Mande uma mensagem">contato</nuxt-link></li>
+        <li><nuxt-link to="/blog" title="Meus artigos">blog</nuxt-link></li>
+      </ul>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
