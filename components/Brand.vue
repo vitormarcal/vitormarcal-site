77809e90436acf59ca8779f426f190b0042dc65b
Vítor Marçal <vitormarcal8@gmail.com>
2019-04-27 09:38:47 -0300

    add marca

---
+++ components/Brand.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1 class="brand">
+    <nuxt-link to="/"> vitor<span>marcal </span></nuxt-link>
+  </h1>
+</template>
+
+<script>
+  export default {
+    name: "Brand"
+  }
+</script>
+
+<style scoped lang="scss">
+
+  .brand {
+    font-family: 'Roboto', sans-serif;
+    font-size: 22px;
+    font-weight: 800;
+    &:before {
+      content: "{";
+      color: black;
+    }
+
+    &:after {
+      content: "}";
+      color: #2e7eb3;
+    }
+
+    a {
+      color: black;
+      text-decoration: none;
+
+      span {
+        color: #2e7eb3;
+      }
+
+      &:hover,
+      &:focus {
+        color: #2e7eb3;
+
+        span {
+          color: black;
+        }
+      }
+    }
+  }
+
+</style>
