1aacbc2a5fcffe972fafe80745773303f9630e65
Vítor Marçal <vitormarcal8@gmail.com>
2019-04-28 10:20:07 -0300

    add http2

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
+
 const compression = require('compression');
 const morgan = require('morgan');
 const express = require('express');
+const http2 = require('http2');
+
+const fs = require('fs')
+const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT = 3000;
 
 app.use(compression());
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
 app.use(express.static('dist'));
 
 
+const options = {
+  cert: fs.readFileSync(path.join(__dirname, '/config/cert.pem')),
+  key: fs.readFileSync(path.join(__dirname, '/config/key.pem'))
+}
 
-app.listen(PORT, () => {
-  console.info(`Listening on: http://localhost:${PORT}/`);
-});
+console.log(options)
+
+http2
+  .createServer(options)
+  .listen(PORT, (err) => {
+    if (err) {
+      throw new Error(err);
+    }
+
+    /!* eslint-disable no-console *!/
+    console.info(`Listening on: http://localhost:${PORT}/`);
+    /!* eslint-enable no-console *!/
+  });
