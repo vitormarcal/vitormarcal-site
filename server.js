0d6eb3f910f715a1e393bbca2138bb38afd179f5
Vítor Marçal <git@vitormarcal.com.br>
2019-04-28 10:25:18 -0300

    delete implementação falha do http2

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
-
 const compression = require('compression');
 const morgan = require('morgan');
 const express = require('express');
-const http2 = require('http2');
-
-const fs = require('fs')
-const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT = 3000;
 
 app.use(compression());
-app.use(morgan('tiny'));
+app.use(morgan('tiny'))
 app.use(express.static('dist'));
 
 
-const options = {
-  cert: fs.readFileSync(path.join(__dirname, '/config/cert.pem')),
-  key: fs.readFileSync(path.join(__dirname, '/config/key.pem'))
-}
 
-console.log(options)
-
-http2
-  .createServer(options)
-  .listen(PORT, (err) => {
-    if (err) {
-      throw new Error(err);
-    }
-
-    /!* eslint-disable no-console *!/
-    console.info(`Listening on: http://localhost:${PORT}/`);
-    /!* eslint-enable no-console *!/
-  });
+app.listen(PORT, () => {
+  console.info(`Listening on: http://localhost:${PORT}/`);
+});
