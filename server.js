8c83172f1bbb333914bd1d33b6ea6be055f6745a
Vítor Marçal <git@vitormarcal.com.br>
2019-04-28 10:50:06 -0300

    add redirect para https

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const PORT = process.env.PORT = 3000;
 
 app.use(compression());
 app.use(morgan('tiny'))
+app.use(function(req, res, next) {
+  if (req.protocol === 'https') {
+    next();
+  } else {
+    console.log('Redirecionado para https');
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
 app.use(express.static('dist'));
 
 
