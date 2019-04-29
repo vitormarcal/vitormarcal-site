fc3a33498f46dcb405d9f19ef7b2aa0269bc870b
Vítor Marçal <git@vitormarcal.com.br>
2019-04-29 13:39:40 -0300

    add esqueleto do live editor

---
+++ pages/blog/Article.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="editor">
+      <textarea v-model="markdown"></textarea>
+    </div>
+    <div v-html="compiledContent"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Article"
+  }
+</script>
+
+<style scoped>
+
+</style>
