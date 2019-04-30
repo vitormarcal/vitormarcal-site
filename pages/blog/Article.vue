d36db4bb48fbcdf10dfac87b1d309a825f7a983d
Vítor Marçal <vitormarcal8@gmail.com>
2019-04-30 13:06:10 -0300

    alguns estilos do editor

--- pages/blog/Article.vue
+++ pages/blog/Article.vue
@@ -1,18 +1,77 @@
 <template>
-  <div>
-    <div class="editor">
-      <textarea v-model="markdown"></textarea>
+  <div class="container">
+    <h1>Novo post</h1>
+    <div class="md">
+      <h2>Entre com o texto em markdown</h2>
+      <textarea rows="16" v-model='md_text'></textarea>
     </div>
-    <div v-html="compiledContent"></div>
+    <div class="preview">
+      <h2>Preview</h2>
+      <div class="preview-text" v-html="previewText"></div>
+    </div>
   </div>
 </template>
 
 <script>
+  let marked = require('marked');
+
   export default {
-    name: "Article"
+    name: "Article",
+    data () {
+      return {
+        md_text: '# Title',
+      }
+    },
+    computed: {
+      previewText() {
+        marked.setOptions({
+          renderer: new marked.Renderer(),
+          gfm: true,
+          tables: true,
+          breaks: true,
+          pedantic: false,
+          sanitize: true,
+          smartLists: true,
+          smartypants: false
+        });
+        return marked(this.md_text)
+      }
+    },
+    methods: {
+      update: function (e) {
+        this.input = e.target.value
+      }
+    }
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+
+  .container {
+    margin-top: 8vh;
+    .md {
+      textarea {
+        width: 100%;
+      }
+    }
+
+    .preview {
+      .preview-text {
+        border: 1px solid #e3e3e3;
+        padding: 15px;
+      }
+    }
+    
+    @media (min-width: 996px) {
+      .md,
+      .preview {
+        width: 50%;
+        float: left;
+        padding-left: 15px;
+        padding-right: 15px;
+      }
+    }
+
+  }
 
 </style>
