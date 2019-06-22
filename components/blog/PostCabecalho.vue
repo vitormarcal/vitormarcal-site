ceff8c8b73d31ed6357be6e51716207d58b1a41e
Vítor Marçal <vitormarcal8@gmail.com>
2019-06-22 15:56:33 -0300

    add componente para o cabecalho do post

---
+++ components/blog/PostCabecalho.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="chamada">
+    <h1>{{attr.title}}</h1>
+    <p>{{attr.description}}</p>
+    <div class="meta" style="margin-top: 2rem">Escrito por {{attr.author}} em {{attr.created_at}}</div>
+    <div class="meta" v-if="attr.obs"  v-html="attr.obs"></div>
+    <div class="meta">
+      <span class="tag" :style="estilos[tag]" v-for="(tag, index) in attr.tags"> #{{tag}} </span>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "PostCabecalho",
+        props: ['attr', 'estilos']
+    }
+</script>
+
+<style scoped lang="scss">
+
+  .meta {
+    margin-top: 5px;
+  }
+
+  .tag {
+    margin: 3px;
+    font-size: 13px;
+    padding: 2px 5px 3px;
+    border-radius: 2px
+  }
+
+
+  .chamada {
+    margin-top: 2rem;
+
+    h1 {
+      line-height: 1.2
+    }
+
+    h1, p, .meta {
+      margin-top: 5px;
+    }
+
+    h1, p {
+      margin-bottom: 1px;
+    }
+
+    p, .meta {
+      color: $gray-dark;
+    }
+
+    .tag__list {
+      margin-top: 5px;
+    }
+  }
+
+</style>
