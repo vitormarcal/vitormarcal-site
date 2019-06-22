00c54cb58eb02f2eb0dae1bab2ab018e7ace1731
Vítor Marçal <vitormarcal8@gmail.com>
2019-06-22 16:29:33 -0300

    add componentes para tag

--- components/blog/ArticleCabecalho.vue
+++ components/blog/ArticleCabecalho.vue
@@ -3,33 +3,22 @@
     <h1>{{attr.title}}</h1>
     <p>{{attr.description}}</p>
     <div class="meta" style="margin-top: 2rem">Escrito por {{attr.author}} em {{attr.created_at}}</div>
-    <div class="meta" v-if="attr.obs"  v-html="attr.obs"></div>
-    <div class="meta">
-      <span class="tag" :style="estilos[tag]" v-for="(tag, index) in attr.tags"> #{{tag}} </span>
-    </div>
+    <div class="meta" v-if="attr.obs" v-html="attr.obs"></div>
+    <CTagArticle v-bind:tags="attr.tags" v-bind:estilos="estilos"/>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "PostCabecalho",
-        props: ['attr', 'estilos']
-    }
+  export default {
+    components: {
+      CTagArticle: () => import('~/components/blog/TagArticle'),
+    },
+    name: "PostCabecalho",
+    props: ['attr', 'estilos']
+  }
 </script>
 
 <style scoped lang="scss">
-
-  .meta {
-    margin-top: 5px;
-  }
-
-  .tag {
-    margin: 3px;
-    font-size: 13px;
-    padding: 2px 5px 3px;
-    border-radius: 2px
-  }
-
 
   .chamada {
     margin-top: 2rem;
@@ -49,10 +38,6 @@
     p, .meta {
       color: $gray-dark;
       font-weight: bold;
-    }
-
-    .tag__list {
-      margin-top: 5px;
     }
   }
 
---
+++ components/blog/TagArticle.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="meta">
+    <span class="tag" :style="estilos[tag]" v-for="(tag, index) in tags"> #{{tag}} </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TagArticle",
+    props: ['tags', 'estilos']
+  }
+</script>
+
+<style scoped>
+
+  .tag {
+    margin: 3px;
+    font-size: 13px;
+    padding: 2px 5px 3px;
+    border-radius: 2px
+  }
+
+  .meta {
+    margin-top: 5px;
+  }
+</style>
