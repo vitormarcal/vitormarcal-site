d7ed9b66c281b3f21eabd124f7ca70ba09bc1547
Vítor Marçal <git@vitormarcal.com.br>
2019-07-09 15:22:01 -0300

    criado paginas de contato e sobre mas com status em construção

---
+++ pages/Contato.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    name: "Contato",
+    layout: 'UnderConstruction',
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/Sobre.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div >
+  </div>
+
+
+</template>
+
+<script>
+
+  export default {
+    name: "Sobre",
+    layout: 'UnderConstruction',
+  }
+</script>
+
+<style scoped lang="scss">
+
+
+
+</style>
