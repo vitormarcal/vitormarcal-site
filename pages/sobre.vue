f7cd9100088b119af2c960b1e459d66fd98c79bc
Vítor Marçal <git@vitormarcal.com.br>
2019-07-10 13:06:41 -0300

    estilos para a pagina de sobre

--- pages/sobre.vue
+++ pages/sobre.vue
@@ -1,11 +1,10 @@
 <template>
 
-  <div class="container">
+  <div>
     <section v-html="whoiam"></section>
     <section v-html="professionalHistory"></section>
     <section v-html="whatIdoToday"></section>
   </div>
-
 
 </template>
 
@@ -20,6 +19,11 @@
       whoiam: () => whoiam.html,
       whatIdoToday: () => whatIdoToday.html,
       professionalHistory: () => professionalHistory.html
+    },
+    head: {
+      bodyAttrs: {
+        class: "bg-color__about"
+      }
     }
   }
 </script>
@@ -27,11 +31,31 @@
 <style scoped lang="scss">
 
   div {
+    @extend .container;
+    margin: 8rem 4rem 4rem;
+    padding-top: 0;
+    background-color: #fff;
+    border: 0 solid rgba(0, 0, 0, .125);
+    border-radius: .625rem;
+    box-shadow: 0 2px 3px 0 rgba(75, 91, 108, .2);
+
     section {
-      @extend .content
+      @extend .content;
+      padding-top: 0;
+    }
+
+    @media (max-width: 765px) {
+      margin: 8rem 1rem 1rem;
+    }
+
+    @media (min-width: 960px) {
+      margin: 8rem 10rem 10rem;
+    }
+
+    @media (min-width: 1201px) {
+      margin: 8rem 20rem 20rem;
     }
   }
-
 
 
 </style>
