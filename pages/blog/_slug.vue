ef48a738000904a374ab19d29f5e969967a53f40
Vítor Marçal <git@vitormarcal.com.br>
2019-07-11 20:36:37 -0300

    delete css morto

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -27,7 +27,7 @@
       } catch (e) {
         let message = 'Ops, algo estranho e inexperado ocorreu';
         let statusCode = 400;
-        if(e.code.startsWith("MODULE_NOT_FOUND")) {
+        if (e.code.startsWith("MODULE_NOT_FOUND")) {
           message = 'Post não encontrado';
           statusCode = 404;
         }
@@ -55,46 +55,4 @@
 </script>
 
 <style scoped lang="scss">
-
-  .content__post {
-    img {
-      transition: transform .2s; /* Animation */
-      width: 100%;
-
-      grid-column: 1 / -1;
-      margin: 20px 0;
-
-
-      &:hover {
-        width: 100%;
-        transform: scale(1.5); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
-      }
-    }
-
-    .content {
-      max-width: 700px;
-      padding-left: 20px;
-      padding-right: 20px;
-      margin: 0 auto;
-
-      .content__post {
-        img {
-          transition: transform .2s; /* Animation */
-          width: 100%;
-
-          grid-column: 1 / -1;
-          margin: 20px 0;
-
-
-          &:hover {
-            width: 100%;
-            transform: scale(1.5); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
-          }
-        }
-
-      }
-
-    }
-
-  }
 </style>
