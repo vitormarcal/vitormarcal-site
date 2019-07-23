046ad7359b4869e263d6e68a67c50b96a0fef700
Vítor Marçal <vitormarcal8@gmail.com>
2019-07-23 13:17:22 -0300

    dividio em colunas

--- pages/curriculo.vue
+++ pages/curriculo.vue
@@ -1,11 +1,15 @@
 <template xmlns="http://www.w3.org/1999/html">
   <div class="curriculo">
-    <h1>{{curriculo.nome}}</h1>
-    <h2>{{curriculo.titulo}}</h2>
+    <div class="item">
+      <h1>{{curriculo.nome}}</h1>
+      <h2>{{curriculo.titulo}}</h2>
+      <c-contato v-bind:contato="curriculo.contato"></c-contato>
+      <c-formacao v-bind:formacao="curriculo.formacao"/>
+    </div>
+    <div class="item">
+      <c-experiencia v-bind:experiencias="curriculo.experiencias"/>
+    </div>
 
-    <c-contato v-bind:contato="curriculo.contato"></c-contato>
-    <c-experiencia v-bind:experiencias="curriculo.experiencias"/>
-    <c-formacao v-bind:formacao="curriculo.formacao"/>
 
   </div>
 </template>
@@ -27,6 +31,29 @@
 </script>
 
 <style scoped lang="scss">
+  .curriculo {
+    margin: 0 auto;
+    padding: 5rem 10rem;
+    border: 1px solid #ccc;
+    display: flex;
+
+    .item {
+      margin: 5px;
+      background: tomato;
+      text-align: left;
+      padding: 1rem;
+      font-size: 1.5em;
+      flex: 1;
+      border: 0 solid rgba(0, 0, 0, .125);
+      border-radius: .625rem;
+      box-shadow: none;
+
+      &:first-child {
+      }
+    }
+
+
+  }
 
 
 </style>
