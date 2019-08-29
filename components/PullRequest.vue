7c80c8ce46f7cd594c70514e02bb633eaeafe7e2
Vítor Marçal <vitormarcal8@gmail.com>
2019-08-29 12:25:10 -0300

    ADD estilos

--- components/PullRequest.vue
+++ components/PullRequest.vue
@@ -2,10 +2,12 @@
 
   <div class="pull-request">
     <span>
-      Contribua com este conteúdo! &#128151;
+      Contribua com este conteúdo!
     </span>
-    <span>Mande sua correção/sugestão, diretamente atravês de um
-      <a v-bind:href="href" title="Mande um Pull Request!" target="_blank" rel="noopener noreferrer"> Pull Request!</a>
+    <span>Mande sua correção ou sugestão, via
+    </span>
+    <span>
+      <a v-bind:href="href" title="Mande um Pull Request!" target="_blank" rel="noopener noreferrer"> Pull Request no Github!&#128151;</a>
     </span>
   </div>
 
@@ -20,14 +22,16 @@
 <style scoped lang="scss">
 
   .pull-request {
+    @extend .texto;
+    font-size: 15px;
     display: flex;
     max-width: 302px;
     border: 0 solid rgba(0, 0, 0, 0.125);
     border-radius: .625rem;
     box-shadow: 0 2px 3px 0 rgba(75, 91, 108, 0.2);
     position: fixed;
-    left: 4rem;
-    bottom: 11%;
+    bottom: 4%;
+    right: 3.5rem;
     flex-direction: column;
     padding: 1rem;
     background-color: #f0d1c4;
