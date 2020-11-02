b957ae1c7fa072de6256a3eadd51cb5f2da164a1
Vítor Marçal <git@vitormarcal.com.br>
2020-11-02 14:10:54 -0300

    refactoring component prject card

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -3,16 +3,7 @@
 
     <ul>
       <template v-for="project in projects">
-        <li class="card">
-          <a :href="project.link" rel="noopener noreferrer" target="_blank">
-            <img :src="project.img" :alt="project.alt">
-          </a>
-          <h2>{{ project.name }}</h2>
-          <p class="description">
-            {{ project.description }}
-          </p>
-          <input type="button" value="Ver projeto">
-        </li>
+        <project-card :project="project"></project-card>
       </template>
     </ul>
 
@@ -22,9 +13,14 @@
 
 <script>
 import {mapState} from 'vuex'
+import ProjectCard from "~/components/portfolio/ProjectCard";
 
 export default {
   name: "portfolio",
+  components: {ProjectCard},
+  comments: {
+    ProjectCard: () => import('~/components/portfolio/ProjectCard')
+  },
   created() {
     this.$store.dispatch('portfolio/setProject')
   },
@@ -50,69 +46,6 @@ export default {
     gap: 20px;
     list-style: none;
     padding: 0;
-
-    .card {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      margin-bottom: 2rem;
-      border-width: 1px;
-      border-style: solid;
-      border-color: rgb(219, 229, 230);
-      overflow: hidden;
-      padding-bottom: 1rem;
-      box-shadow: 0 1rem 1.5rem -.3rem rgba(76, 114, 179, .2);
-
-      img {
-        cursor: pointer;
-        display: block;
-        object-fit: cover;
-        width: 100%;
-        height: 100%;
-        transition: transform 0.3s ease 0s;
-        border-bottom: 1px solid rgb(219, 229, 230);
-        box-shadow: 0 1rem 1.5rem -.3rem rgba(76, 114, 179, .2);
-
-        &:hover {
-
-          transform: scale(1.05);
-        }
-
-      }
-      h1 {
-        font-size: 25px;
-        font-family: 'Roboto', sans-serif;
-        font-weight: normal;
-        color: #000000;
-        margin: 2rem 0 0;
-      }
-
-      p {
-        padding: 0 1rem 0 1rem;
-      }
-
-      input {
-        cursor: pointer;
-        background-color: $blue-light;
-        width: 240px;
-        height: 40px;
-        font-size: 16px;
-        line-height: 16.34px;
-        color: #ebfbff;
-        text-align: center;
-        font-weight: bold;
-        border-radius: 30px;
-        box-shadow: rgba(0, 0, 0, 0.1) 0 0.3rem 1rem;
-        transition: all 0.3s ease 0s;
-        text-transform: uppercase;
-
-        &:hover {
-          transform: scale(1.015);
-        }
-      }
-
-    }
-
   }
 
 
