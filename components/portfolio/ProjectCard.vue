2f174be617ed7421f38e162aec52e6e4fea0d675
Vitor Marçal <vitormarcal8@gmail.com>
2020-11-02 14:32:40 -0300

    refactoring component prject card

--- components/portfolio/ProjectCard.vue
+++ components/portfolio/ProjectCard.vue
@@ -1,6 +1,6 @@
 <template>
   <li class="card">
-    <a :href="project.link" :title="'Página do projeto' + project.name" rel="noopener noreferrer" target="_blank">
+    <a :href="project.link" :title="'Página do projeto ' + project.name" rel="noopener noreferrer" target="_blank">
       <img :src="project.img" :alt="project.alt">
 
       <h2>{{ project.name }}</h2>
