1da3855c7365d879302f034cf87d3f2c9b71e4a5
Vítor Marçal <git@vitormarcal.com.br>
2020-11-02 11:57:46 -0300

    problema de link em prod

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -4,7 +4,7 @@
     <ul>
       <li class="card">
         <a href="https://huddle-landing-page.vitormarcal.com.br/" rel="noopener noreferrer" target="_blank">
-          <img src="/images/huddle-landig-page.png" alt="Desktop design screenshot for project">
+          <img src="/images/huddle-landig-page.jpg" alt="Desktop design screenshot for project">
         </a>
         <p class="description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
